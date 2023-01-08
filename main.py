afafa2ee0d17b4feb871aa393509e7d5a47daa9d
Elijah <110748927+Kalibias@users.noreply.github.com>
2023-01-08 01:10:14 -0500

    Update main.py
    Moved duplicate variables for even more simplicity.

--- main.py
+++ main.py
@@ -53,41 +53,38 @@ def tarot_draw():
         tarot = json.load(tarot)
     # Picks the arcana that'll be used to draw the card.
     arcana = random.choice(tarot_arcana)
-    
+    facing = random.choice(card_facing)
+    meaning = 'key_means' if facing == "Upright" else 'rev_means'
     if arcana == "Major":
-        
-        maj_arc = random.choice(list(maj_dict.keys()))
-        facing = random.choice(card_facing)
 
+        maj_arc = random.choice(list(maj_dict.keys()))
+        
         # Renamed variables for better clarity of what they do.
         drawn_card = f"You card is {maj_arc} \n{facing}"
-        
+
         # Utilizing facing variable to define 'meaning.'
-        meaning = 'key_means' if facing == "Upright" else 'rev_means'
+
         result = tarot["Major"][int(maj_dict[maj_arc])][maj_arc][0][meaning]
 
         # Makes the card readable in the console.
         card = "\n".join(textwrap.wrap(result, 90))
-        print(f"{drawn_card}\n{card}")
-        return f"{drawn_card}\n{card}"
 
     elif arcana == "Minor":
 
         min_arc = random.choice(list(min_dict.keys()))
-        facing = random.choice(card_facing)
-        
+
         #Added Variable for Minor Arcana.
         suit = random.choice(list(suit_dict.keys()))
-        
+
         # Adds the Suit variable into the Drawn Card
         drawn_card = f"You card is {suit} of {min_arc} \n{facing}"
-        meaning = 'key_means' if facing == "Upright" else 'rev_means'
 
         # Suit Dict comes in to easily define which suit was drawn.
         result = tarot["Minor"][min_arc][suit_dict[suit]][suit][0][meaning]
         card = "\n".join(textwrap.wrap(result, 90))
-        print(f"{drawn_card}\n{card}")
-        return f"{drawn_card}\n {card}"
+
+    print(f"{drawn_card}\n{card}")
+    return f"{drawn_card}\n {card}"
 
 # For now it'll draw a singular card.
 tarot_draw()
